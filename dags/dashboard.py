0733da3582e098b71b25f9494486dee89f93f21f
Pongtanat <106550917+pongtanat33@users.noreply.github.com>
2025-10-03 04:05:47 +0000

    .

---
+++ dags/dashboard.py
@@ -0,0 +1,80 @@
+import datetime
+from airflow.providers.standard.operators.python import PythonOperator
+from airflow.providers.standard.operators.empty import EmptyOperator
+from airflow.providers.standard.operators.http import HttpOperator
+from airflow.sdk import DAG
+
+
+def _extract_api():
+    print("Extract Api")
+
+def _extract_postgres():
+    print("Extract Postgres")
+
+def _extract_google_analytics():
+    print("Extract Google Analytics")
+
+def _transfrom_and_clean_data():
+    print("_transfrom_and_clean_data")
+
+def _load_data_warhouse():
+    print("Load Data Warehouse")
+
+def _generate_report():
+    print("Generate Data")
+
+
+
+
+with DAG(
+    dag_id="dashboard",
+    start_date=datetime.datetime(2025, 10, 1),
+    schedule=None,
+):
+    
+    start= EmptyOperator(task_id="start")
+
+    extract_api = PythonOperator(
+        task_id="extract_api",
+        python_callable=_extract_api
+    )
+
+    extract_postgress = PythonOperator(
+        task_id = "extract_postgres",
+        python_callable = _extract_postgres
+    )
+
+    extract_google_analytics = PythonOperator(
+        task_id = "extract_google_analytics",
+        python_callable = _extract_google_analytics
+    )
+
+    transform_clean_data = PythonOperator(
+        task_id = "transform_clean_data",
+        python_callable = _transfrom_and_clean_data
+    )
+
+    load_to_data_warehouse = PythonOperator(
+        task_id = "load_data_warhouse",
+         python_callable = _load_data_warhouse
+    )
+
+    generate_report = PythonOperator(
+        task_id = "generate_report",
+         python_callable = _generate_report
+    )
+
+    task_get_op = HttpOperator(
+        task_id="get_op",
+        method="GET",
+        endpoint="https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m",
+        data={"param1": "value1", "param2": "value2"},
+        headers={},
+        dag=dag,
+    )
+
+
+    end = EmptyOperator(task_id="end")
+
+
+    start >> [extract_api,extract_postgress,extract_google_analytics]  >> transform_clean_data >> load_to_data_warehouse  >> generate_report >> end
--- dags/everysunday.py
+++ dags/everysunday.py
@@ -1,13 +1,10 @@
 import datetime
-from zoneinfo import ZoneInfo
 from airflow.sdk import DAG
 from airflow.utils import timezone
 from airflow.providers.standard.operators.empty import EmptyOperator
 from airflow.operators.python import PythonOperator
 from airflow.operators.bash import BashOperator
-from pendulum import timezone
 
-local_tz = timezone("Asia/Bangkok")
 
 def _hello():
     print("hello world")
@@ -16,8 +13,7 @@ def _hello():
 dag = DAG(
     "everysunday",
     schedule="* * * * 7",
-    start_date=local_tz.datetime(2025, 10, 2, 0, 0),
-    timezone=local_tz,
+    start_date= timezone.datetime(2025,10,2),
 )
 
 hello = PythonOperator(
